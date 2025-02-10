460a57589dc98e42eb9934aa2c7e6fc59193bee1
SaintTonka <daniilgavrilov9994@gmail.com>
2025-02-10 15:15:19 +0300

    removing unnec. try-ex

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
 @app.get("/")
 def greetings(name: str = "Recruto", msg: str = "Давай дружить!") -> PlainTextResponse:
-    try:
-        return PlainTextResponse(
-            content=f"Hello {name}! {msg}", 
-            media_type="text/plain; charset=utf-8"
-        )
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error")
+    return PlainTextResponse(
+        content=f"Hello {name}! {msg}",
+        media_type="text/plain; charset=utf-8"
+    )
