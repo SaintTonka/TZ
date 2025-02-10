031121f7498501a63ed8a49f07abaa21a9c9f129
SaintTonka <daniilgavrilov9994@gmail.com>
2025-02-10 14:31:18 +0300

    codirovka fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
-def greetings(name:str = "Recruto", msg:str = "Давай дружить!") -> str:
+def greetings(name: str = "Recruto", msg: str = "Давай дружить!") -> dict:
     try:
-        return f"Hello {name}! {msg}"
+        return JSONResponse(content={"message": f"Hello {name}! {msg}"}, media_type="application/json; charset=utf-8")
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
